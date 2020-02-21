2d2825e5646d2ca3c524f42b29ed51236673d26c
mike <mikemcfaddin@gmail.com>
2020-02-21 09:50:19 -0500

    adds backend restful routes and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# places = Place.create([
+#   {name: "Ted Johnson", address: "120 Spring Ave., Los Angeles, CA"}, 
+#   {name: "Olivia Barker", address: "42 Space Lane, Mont Claire, NJ"},
+#   {name: "Tina Smith", address: "18 George St., Orlando, FL"},
+#   {name: "Mike Edwards", address: "145 Jackson Ave., Greenville, SC"},
+#   {name: "Troy Evans", address: "1506 Ruby Circle, Auburn, MA"},
+# ])
